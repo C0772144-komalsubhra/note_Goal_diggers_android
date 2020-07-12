48c12ecb90ed589c303552980b80919273ad2d30
gagan <sherry2312.gk@gmail.com>
2020-07-12 00:55:24 -0400

    fragment added in maps activity

--- app/src/main/java/com/example/note_goal_diggers_android/mapsActivity.java
+++ app/src/main/java/com/example/note_goal_diggers_android/mapsActivity.java
@@ -1,9 +1,16 @@
 package com.example.note_goal_diggers_android;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
 
 public class mapsActivity extends AppCompatActivity {
     SupportMapFragment supportMapFragment;
@@ -13,6 +20,34 @@ public class mapsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps);
+        setContentView(R.layout.activity_maps); supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.google_map);
+
+        Intent i = getIntent();
+        lat = i.getExtras().getString("notelat");
+        lon = i.getExtras().getString("notelon");
+
+        getcurrentLoaction();
+
+
     }
+
+    private void getcurrentLoaction() {
+
+        supportMapFragment.getMapAsync(new OnMapReadyCallback() {
+            @Override
+            public void onMapReady(GoogleMap googleMap) {
+                LatLng latLng = new LatLng(Double.parseDouble(lat),Double.parseDouble(lon));
+
+                MarkerOptions options = new MarkerOptions().position(latLng).title("im  here");
+
+                googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+
+                googleMap.addMarker(options);
+            }
+        });
+
+
+    }
+
+
 }
