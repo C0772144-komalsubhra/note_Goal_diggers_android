01037dc3d56b8db766b4e355c6d5264c5d00865d
gagan <sherry2312.gk@gmail.com>
2020-07-11 22:15:05 -0400

    pushed all files

---
+++ app/src/main/java/com/example/note_goal_diggers_android/adapterSubject.java
@@ -0,0 +1,114 @@
+package com.example.note_goal_diggers_android;
+
+import android.content.Context;
+import android.content.Intent;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class adapterSubject extends RecyclerView.Adapter< adapterSubject.ViewHolder> implements Filterable {
+
+    LayoutInflater inflater;
+    List<subject> subjectlist =new ArrayList<>();
+    List<subject> subjectlistFull =new ArrayList<>();
+
+    public adapterSubject(Context context, List<subject> subjectlist){
+        this.inflater = LayoutInflater.from(context);
+        this.subjectlist = subjectlist;
+        subjectlistFull = new ArrayList<subject>(subjectlist);
+
+    }
+    @NonNull
+    @Override
+    public adapterSubject.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = inflater.inflate(R.layout.custom_subjectcell,parent,false);
+        return new ViewHolder(view );
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull adapterSubject.ViewHolder holder, int position) {
+        String title = subjectlist.get(position).getSubjectID();
+        holder.subtitle.setText(title);
+
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return  subjectlist.size();
+    }
+
+    @Override
+    public Filter getFilter() {
+        return subjectfilter;
+    }
+
+    private Filter subjectfilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            List<subject> filteredList = new ArrayList<subject>( );
+            if(constraint == null || constraint.length() == 0){
+                filteredList.addAll(subjectlistFull );
+            }else {
+                String filterpattern = constraint.toString().toLowerCase().trim();
+
+                for(subject item :subjectlistFull){
+                    if(item.getSubjectID().toLowerCase().contains(filterpattern)){
+                        filteredList.add(item);
+                    }
+                }
+            }
+            FilterResults results = new FilterResults();
+            results.values = filteredList;
+
+            return results;
+
+        }
+
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+
+            subjectlist.clear();
+            Log.d("searchresults", String.valueOf(results.values));
+
+            subjectlist.addAll((List) results.values);
+            notifyDataSetChanged();
+
+        }
+    };
+
+
+    public class ViewHolder extends RecyclerView.ViewHolder{
+        TextView subtitle;
+
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            subtitle = itemView.findViewById(R.id.subjectTitle);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                    Intent i = new Intent(v.getContext(), MainActivity.class);
+                    i.putExtra("subname",String.valueOf(subjectlist.get(getAdapterPosition()).getSubjectID()));
+                    v.getContext().startActivity(i);
+
+                    Toast.makeText(v.getContext(),String.valueOf(subjectlist.get(getAdapterPosition()).getSubjectID()), Toast.LENGTH_LONG).show();
+
+                }
+            });
+        }
+    }
+}
---
+++ app/src/main/java/com/example/note_goal_diggers_android/subject.java
@@ -0,0 +1,47 @@
+package com.example.note_goal_diggers_android;
+
+import java.util.ArrayList;
+
+public class subject {
+    private String SubjectID;
+    private ArrayList<notes> subjectnotes = new ArrayList<notes>();
+
+
+
+    public subject(String subjectID ) {
+        SubjectID = subjectID;
+
+
+    }
+
+    public subject() {
+    }
+
+
+
+    public String getSubjectID() {
+        return SubjectID;
+    }
+
+    public void setSubjectID(String subjectID) {
+        SubjectID = subjectID;
+    }
+
+
+
+
+    public void addnotes(notes note)
+    {
+        this.subjectnotes.add(note);
+    }
+
+    public ArrayList<notes> getSubjectnotes() {
+        return subjectnotes;
+    }
+
+    public void setSubjectnotes(ArrayList<notes> subjectnotes) {
+        this.subjectnotes = subjectnotes;
+    }
+}
+
+
