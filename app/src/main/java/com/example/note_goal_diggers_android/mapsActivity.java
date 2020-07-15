b5a2f33302332d2df5a8ced4b0aed031f2739cc5
gagan <sherry2312.gk@gmail.com>
2020-07-15 18:22:05 -0400

    modifying the maps

--- app/src/main/java/com/example/note_goal_diggers_android/mapsActivity.java
+++ app/src/main/java/com/example/note_goal_diggers_android/mapsActivity.java
@@ -1,9 +1,12 @@
 package com.example.note_goal_diggers_android;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
@@ -11,7 +14,13 @@ import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 import java.util.HashMap;
 
@@ -30,17 +39,55 @@ public class mapsActivity extends AppCompatActivity {
 
 
 
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_maps); supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.google_map);
+        setContentView(R.layout.activity_maps);
+
+        supportMapFragment = (SupportMapFragment)getSupportFragmentManager().findFragmentById(R.id.google_map);
 
         Intent i = getIntent();
         lat = i.getExtras().getString("notelat");
         lon = i.getExtras().getString("notelon");
+        final String  id = i.getExtras().getString("id");
+        final String sname = i.getExtras().getString("sname");
 
-        getcurrentLoaction();
+//        HashMap<String,Double> hm = (HashMap<String, Double>) i.getExtras().get("hashmap");
+//        Log.d("myhash", String.valueOf(hm));
+
+        notesdata = FirebaseDatabase.getInstance().getReference().child("Notes").child(sname).child("subjectnotes").child(id);
+        notesdata.addValueEventListener(new ValueEventListener() {
+
+
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+
+                if(String.valueOf(dataSnapshot.child("mulmaps").getValue()).equals("0")){
+                    newlat = 0.0;
+                    newlong = 0.0;
+                    getcurrentLoaction();
+                }else {
+
+                    Log.d("tryingtodouble", String.valueOf(dataSnapshot.child("mulmaps").getValue()));
+                    getlatlong = (HashMap<String, Double>) dataSnapshot.child("mulmaps").getValue();
+
+                    newlat = getlatlong.get("latitude");
+                    newlong = getlatlong.get("longitude");
+
+                    getcurrentLoaction();
+
+                }
+            }
+
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
+
+
 
 
     }
@@ -50,18 +97,47 @@ public class mapsActivity extends AppCompatActivity {
         supportMapFragment.getMapAsync(new OnMapReadyCallback() {
             @Override
             public void onMapReady(GoogleMap googleMap) {
+                mymap = googleMap;
                 LatLng latLng = new LatLng(Double.parseDouble(lat),Double.parseDouble(lon));
 
-                MarkerOptions options = new MarkerOptions().position(latLng).title("im  here");
+                MarkerOptions options = new MarkerOptions().position(latLng).title("Created here!");
 
                 googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
 
                 googleMap.addMarker(options);
+
+                if(newlat != 0.0 && newlong !=0.0) {
+
+
+
+
+                    LatLng edited = new LatLng(newlat, newlong);
+
+                    MarkerOptions edtmark = new MarkerOptions().position(edited).title("edited here");
+
+                    //   googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng,10));
+
+
+                    googleMap.addMarker(edtmark);
+
+                    drawPolyline(latLng,edited);
+
+                }
+
             }
         });
 
 
     }
 
+    private void drawPolyline(LatLng latLng, LatLng latLng1) {
+        Polyline line = mymap.addPolyline(new PolylineOptions()
+                .add(latLng,latLng1)
+                .width(10)
+                .color(Color.BLUE));
+        line.setClickable(true);
+
+
+    }
 
 }
